7ee1cda99f39c22f34547b5c24ef76168db9d4bc
miyagipipi <66454376+miyagipipi@users.noreply.github.com>
2020-11-27 14:22:30 +0800

    封装了程序

---
+++ runReport.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 27 14:04:13 2020
+
+@author: 李启晟
+"""
+
+import AutoWeekReport as awr
+
+if __name__ == '__main__':
+    path = input('请输入产值表excel的路径: ')
+    month = input('请输入月份： ')
+
+    startData = input('请输入起始日期（注意此处的日期格式必须为xxxx-xx-xx）: \n')
+    endDate = input('请输入结束日期（注意此处的日期格式必须为xxxx-xx-xx）: \n')
+
+    a = awr.AutoWeekReport(path, month, startData, endDate)
